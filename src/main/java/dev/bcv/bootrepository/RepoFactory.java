7eccbe98fbeb7a11ec6acf7302eb3ad2767b9e58
Baris Vural <bariscan.vural@bloomreach.com>
2018-08-12 08:39:09 +0200

    Use pwd instead of temp directory

--- src/main/java/dev/bcv/bootrepository/RepoFactory.java
+++ src/main/java/dev/bcv/bootrepository/RepoFactory.java
@@ -8,7 +8,7 @@ public class RepoFactory {
     private static final String repoConfig = "/dev/bcv/bootrepository/repository-mysql.xml";
 
     public static BootRepository getHippoEnterpriseRepository() throws RepositoryException {
-        final File tmpdir = new File(System.getProperty("java.io.tmpdir"));
+        final File tmpdir = new File(System.getProperty("user.dir"));
         final File storage = new File(tmpdir, "storage");
         if (!storage.exists()) {
             storage.mkdir();
